92691337455880f07b861116abd1eba6f7a2c680
Ziddi C.J. <zidthesunkid@gmail.com>
2026-06-30 22:46:05 -0600

    Created secret /ziddy-secret route with exclusive dashboard

---
+++ app/ziddy-secret/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import FemboyAgentDashboard from '../femboy-agent-dashboard';
+
+export default function ZiddySecret() {
+  return (
+    <div className="min-h-screen bg-black">
+      <FemboyAgentDashboard />
+    </div>
+  );
+}
